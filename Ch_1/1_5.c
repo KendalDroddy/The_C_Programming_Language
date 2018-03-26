c89c18928b7918147049eeddd8164d491679caf5
Kendal Droddy <kdroddy001@gmail.com>
2018-03-25 21:53:20 -0500

    Refactored to include symbolic constants

--- Ch_1/1_5.c
+++ Ch_1/1_5.c
@@ -5,6 +5,10 @@ from 300 degrees to 0.
 *************************************************/
 #include <stdio.h>
 
+#define LOWER 0 	/* lower limit of the table */
+#define UPPER 300 	/* upper limit */
+#define STEP  20	/* step size */
+
 /* Print Fahrenheit-Celsius table */
 int main()
 {
@@ -12,7 +16,7 @@ int main()
 
 	printf("\nFahrenheit-Celsius Table\n");
 	printf("----------------------------\n");
-	for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+	for(fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
 	{
 		printf(" %3d\t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 	}

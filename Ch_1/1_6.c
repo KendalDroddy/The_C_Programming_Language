cb79c8604e16b013849b1f1ba5efed94a745bb2e
Kendal Droddy <kdroddy001@gmail.com>
2018-03-26 22:30:30 -0500

    Refactored program

--- Ch_1/1_6.c
+++ Ch_1/1_6.c
@@ -4,21 +4,14 @@ is 0 or 1
 *************************************************/
 #include <stdio.h>
 
-#define LOWER 0 	/* lower limit of the table */
-#define UPPER 300 	/* upper limit */
-#define STEP  20	/* step size */
-
-/* Print Fahrenheit-Celsius table */
 int main()
 {
-	/* Copy input to output - 2nd version */
 	int c;
-	
-	c = getchar();
+
 	while((c = getchar()) != EOF)
 	{
-		printf("EOF Value: %d ", c != EOF);
+		printf("%d ", c != EOF);
 		putchar(c);
-		c = getchar();
 	}
+	printf("\n%d\n", c != EOF);
 }

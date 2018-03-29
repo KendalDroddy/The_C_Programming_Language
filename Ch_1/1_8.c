723b0f87a3b75d88196f9c1711df6e9726d1758b
Kendal Droddy <kdroddy001@gmail.com>
2018-03-28 22:48:03 -0500

    Count blanks, tabs, and newlines per program specifications

--- Ch_1/1_8.c
+++ Ch_1/1_8.c
@@ -1,16 +1,21 @@
 /*************************************************
-Write a program to print the value of EOF
+Write a program to count blanks, tabs, & newlines
 *************************************************/
 #include <stdio.h>
 
-/* Count lines in input */
+/* Count blanks, tabs & newlines */
 int main()
 {
-	int c, nl;
+	int c, b = 0, t = 0, nl = 0;
 
-	nl = 0;
 	while((c = getchar()) != EOF)
+	{
 		if(c == '\n')
 			++nl;
-	printf("%d\n", nl);
+		if(c == '\t')
+			++t;
+		if(c == ' ')
+			++b;
+	}
+	printf("Blanks: %d Tabs: %d Newlines: %d\n", b, t, nl);
 }
